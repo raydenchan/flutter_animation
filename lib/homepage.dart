883f8d8331537ec6fb600967a89cf52ddc10a8f5
raydenleo <squallray@hotmail.com>
2024-12-13 01:23:18 +0800

    created lottie animation on homepage

---
+++ lib/homepage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Lottie.asset('assets/Animation - 1734023238139.json'),
+    ));
+  }
+}
